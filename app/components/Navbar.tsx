8fdece5cc3da65cae0a2d093d1f230af9cb510cd
Monosij Kayal <monosijkayal@gmail.com>
2025-10-13 21:20:27 +0530

    Refactor Navbar component for improved layout and text styling

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -17,17 +17,17 @@ export default function Navbar() {
   ];
 
   return (
-    <nav className="sticky top-0 z-50 bg-white border-b border-gray-100">
-      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center h-16">
+    <nav className="sticky top-0 z-50 bg-white border-b border-gray-100 ">
+      <div className="max-w-2xl mx-auto sm:px-6 lg:px-8 flex justify-center items-center h-16 border-r-1 border-l-1 border-gray-100">
         {/* Desktop Menu */}
-        <div className="hidden md:flex space-x-8">
+        <div className="hidden md:flex space-x-8 justify-between items-center ">
           {navItems.map((item) => {
             const isActive = pathname === item.path;
             return (
               <Link
                 key={item.path}
                 href={item.path}
-                className={`font-medium transition-all duration-300 ${
+                className={`font-extralight text-sm transition-all duration-300 ${
                   isActive
                     ? "text-black "
                     : "text-gray-400 hover:text-black"
@@ -37,6 +37,9 @@ export default function Navbar() {
               </Link>
             );
           })}
+          <div className ="px-2 rounded-md bg-gray-200 text-gray-500 hover:text-gray-800 cursor-pointer">
+            <button  className = "text-sm cursor-pointer">Login</button>
+          </div>
         </div>
 
         {/* Mobile Menu Button */}
@@ -58,7 +61,7 @@ export default function Navbar() {
                 key={item.path}
                 href={item.path}
                 onClick={() => setMenuOpen(false)}
-                className={`block font-medium transition-all duration-300 ${
+                className={`block font-extralight text-sm transition-all duration-300 ${
                   isActive
                     ? "text-black scale-105"
                     : "text-gray-500 hover:text-black"
